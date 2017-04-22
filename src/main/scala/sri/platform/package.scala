5f8f5175a120189718a0e61324ae34554609cbac
chandu0101 <chandu.csu2010@gmail.com>
2017-04-23 04:08:31 +0530

    update deps and rename SriPlatForm to SriPlatform

--- src/main/scala/sri/platform/package.scala
+++ src/main/scala/sri/platform/package.scala
@@ -2,7 +2,7 @@ package sri
 
 package object platform {
 
-  implicit object SriPlatForm extends sri.universal.SriPlatForm {
+  implicit object SriPlatform extends sri.universal.SriPlatform {
     @inline def isIOS: Boolean = false
     @inline def isAndroid: Boolean = false
     @inline def isWeb: Boolean = true
